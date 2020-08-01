ff918a4f489520dd9eaffeb296de72fb24a84660
DHB <1084714805@qq.com>
2020-08-02 00:44:00 +0800

    refine PT(ELS)

--- Path Tracing Advanced/main.glsl
+++
@@ -1,8 +0,0 @@
-#define GAMMA 2.2f
-
-void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
-	vec2 uv = fragCoord.xy / iResolution.xy;
-	vec3 color = texture(iChannel0, uv).rgb;
-    
-	fragColor = vec4(clamp(pow(color, vec3(1./GAMMA)), 0., 1.), 1.);
-}
--- Path Tracing Explicit Light Sampling/BufA.glsl
+++ Path Tracing Explicit Light Sampling/BufA.glsl
@@ -38,6 +38,9 @@ struct Plane {
 // Util functions
 float seed = 0.;
 float rand() { return fract(sin(seed++)*43758.5453123); }
+int randInt(int min, int max) {
+    return min + int(floor(rand() * (.99999 + float(max - min))));
+}
 
 vec3 cosWeightedSampleHemisphere(vec3 n) {
     float u1 = rand(), u2 = rand();
@@ -64,15 +67,21 @@ vec3 cosWeightedSampleHemisphere(vec3 n) {
 }
 
 // Scene Description
-#define NUM_SPHERES 3
+#define NUM_SPHERES 4
 #define NUM_PLANES 6
+#define NUM_EMISSIVE_SPHERES 2
 Sphere spheres[NUM_SPHERES];
 Plane planes[NUM_PLANES];
+int emissiveSphereIndexs[NUM_EMISSIVE_SPHERES];
 
 void initScene() {
     spheres[0] = Sphere(16.5, vec3(27,16.5,47), Material(SPEC, vec3(0.), vec3(1.), 0.));
     spheres[1] = Sphere(16.5, vec3(73, 16.5, 78), Material(REFR, vec3(0.), vec3(.75, 1., .75), 1.5));
-    spheres[2] = Sphere(600., vec3(50, 689.3, 50), Material(DIFF, vec3(6.), vec3(0.), 0.));
+    spheres[2] = Sphere(3., vec3(30., 80., 50), Material(DIFF, vec3(70.), vec3(0.), 0.));
+    spheres[3] = Sphere(3., vec3(80., 80., 50), Material(DIFF, vec3(70.), vec3(0.), 0.));
+    
+    emissiveSphereIndexs[0] = 2;
+    emissiveSphereIndexs[1] = 3;
     
 	planes[0] = Plane(vec3(0, 0, 0), vec3(0, 1, 0), Material(DIFF, vec3(0.), vec3(.75), 0.));
     planes[1] = Plane(vec3(-7, 0, 0), vec3(1, 0, 0), Material(DIFF, vec3(0.), vec3(.75, .25, .25), 0.));
@@ -129,7 +138,7 @@ int intersect(Ray ray, out float t, out vec3 normal, out Material mat) {
     for (int i = 0; i < NUM_PLANES; i++) {
         float d = intersectPlane(ray, planes[i]);
         if (d != 0. && d < t) {
-            id = i;
+            id = NUM_SPHERES + i;
             t = d;
             normal = planes[i].normal;
             mat = planes[i].mat;
@@ -152,10 +161,44 @@ Ray generateRay(vec2 uv) {
     return Ray(camPos, normalize(.5135 * (aspectRatio * p.x * cx + p.y * cy) + cz));
 }
 
+// 显式球型光源采样
+vec3 explicitLightSampling(vec3 pos, vec3 normal) {
+    int index = emissiveSphereIndexs[randInt(0, NUM_EMISSIVE_SPHERES - 1)];
+    Sphere s = spheres[index];
+    
+    vec3 l0 = s.pos - pos;
+    
+    float cos_a_max = sqrt(1. - s.radius * s.radius / dot(l0, l0));
+    float eps1 = rand(), eps2 = rand();
+    float cos_a = 1. - eps1 + eps1 * cos_a_max;
+    float sin_a = sqrt(1. - cos_a * cos_a);
+    float phi = 2. * PI * eps2;
+    
+    vec3 sw = normalize(l0);
+    vec3 su = normalize(cross(sw.yzx, sw));
+    vec3 sv = cross(sw, su);
+    
+    vec3 l = cos(phi) * sin_a * su + sin(phi) * sin_a * sv + cos_a * sw;
+    l = normalize(l);
+    
+    float t;
+    vec3 n;
+    Material mat;
+    int id = intersect(Ray(pos + l * RAY_EPSILON, l), t, n, mat);
+    if (id == index) {
+        float omega = 2. * PI * (1. - cos_a_max);
+        vec3 e = mat.emission * (clamp(dot(normal, l), 0., 1.) * omega / PI);
+        return e * float(NUM_EMISSIVE_SPHERES);
+    }
+    
+    return vec3(0.);
+}
+
 // 辐射度计算
 vec3 trace(Ray ray) {
     vec3 radiance = vec3(0.);		 // 累积辐射度
     vec3 reflectance = vec3(1.);	 // 累积反射率
+    float E = 1.;
     for (int depth = 0; depth < MAX_DEPTH; depth++) {
         float t;	    // 相交处距离
         vec3 n;			// 相交面的法线
@@ -171,7 +214,8 @@ vec3 trace(Ray ray) {
         
         
         // 累加这一次的辐射度
-        radiance += reflectance * mat.emission;
+        radiance += reflectance * mat.emission * E;
+        E = 1.;
         
         vec3 color = mat.color;
         // 反射率最大分量
@@ -189,8 +233,14 @@ vec3 trace(Ray ray) {
 		
         
         if (mat.refl == DIFF) {				// 漫反射表面
-            ray.dir = cosWeightedSampleHemisphere(nl);
+            vec3 luminance = explicitLightSampling(ray.origin, nl);
+            
             reflectance *= color;
+            radiance += reflectance * luminance;
+
+            // 取消下一次的自发光采样(已经在explicit light sampling中完成)
+            E = 0.;        
+            ray.dir = cosWeightedSampleHemisphere(nl);
         } else if (mat.refl == SPEC) {	    // 镜面反射表面
             ray.dir = reflect(ray.dir, n);
             reflectance *= color;
@@ -238,7 +288,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
     initScene();
     
     vec3 color = vec3(0.);
-    // 超采样
+    // 像素内超采样
     for (int x = 0; x < SUB_SAMPLES; x++) {
         for (int y = 0; y < SUB_SAMPLES; y++) {
         	// Tent Filter
@@ -265,5 +315,6 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
     
     // muiltpass 多次采样算出平均结果
 	color += texture(iChannel0, uv).rgb * float(iFrame);
+    
 	fragColor = vec4(color / float(iFrame + 1), 1.);
 }
